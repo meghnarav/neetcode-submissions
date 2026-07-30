b3f4d01d370196177dfecc5946d3e90348276a98
Meghna <meghnaravikumar06@gmail.com>
2026-07-30 23:49:50 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min=prices[0]; int max=0; int n=prices.length;
+        for(int i=0; i<n; i++){
+            if(prices[i]<min) min=prices[i];
+            else if((prices[i]-min) > max) max=prices[i]-min;
+        }
+        return max;
+    }
+}
